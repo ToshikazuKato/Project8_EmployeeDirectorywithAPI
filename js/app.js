4ba5786abae97122770d2d2b7123cb570542a98b
ToshikazuKato <kazu1427bsk@gmail.com>
2018-09-04 20:46:06 +0530

    Added search function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 
 const directory = document.getElementById("directory");
+const search = document.getElementById("search");
 
 // connect to API and fetch data
 const connectToAPIs = () =>{
-  fetch('https://randomuser.me/api/?results=12')
+  fetch('https://randomuser.me/api/?results=12&nat=us,au,br,ca,us,nz')
   .then(response => response.json())
   .then(data => {
     const empList = data.results;
     createEmployee(empList);
     $('.modal').modaal();
+    searchEmp();
   })
 };
 
@@ -29,12 +31,13 @@ const createEmployee = (emp) =>{
                   <div id="${empName}" style="display:none;">
                     <div class="modalContainer">
                       <img src="${emp.picture.medium}" alt="${empName}" class="img">
-                      <h3 class="name">${empName}</h3>
+                      <h3 class="nameModal">${empName}</h3>
                       <p class="emailModal">${emp.email}</p>
                       <p class="placeModal">${emp.location.city}</p>
                       <hr>
                       <p class="cell">${emp.cell}</p>
                       <p class="address">${emp.location.street},${emp.location.state}, ${emp.location.postcode}</p>
+                      <p class="birthday">Birthday: ${emp.dob.date.slice(5,7)}/${emp.dob.date.slice(8,10)}/${emp.dob.date.slice(0,4)}</p>
                     </div>
                   </div>
                 </a>`;
@@ -42,6 +45,33 @@ const createEmployee = (emp) =>{
   directory.innerHTML = empHTML;
 }
 
+const searchEmp = () => {
+
+  search.addEventListener('keyup', (event)=>{
+    event.preventDefault();
+    const name = document.getElementsByClassName('name');
+    const searchedWord = search.value.toUpperCase();
+    const href = document.getElementsByClassName('modal');
+
+    // get employee's name from objects
+    const empArrForSearch = Object.keys(name).map((val,index) => {
+      return name[index].innerHTML.toUpperCase();
+    });
+
+    // search function
+    empArrForSearch.forEach((val,index)=>{
+
+      if( val.indexOf(searchedWord) > -1 ){
+        href[index].style.display = 'block';
+      }else{
+        href[index].style.display = 'none';
+
+      }
+
+    });
+
+  });
+}
 
 connectToAPIs();
 

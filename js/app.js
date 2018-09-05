79e74b21152e8d9f28f02b39e40dfabf59cfe654
ToshikazuKato <kazu1427bsk@gmail.com>
2018-09-06 01:21:10 +0530

    Remove jQuery and create modal
    Remove jQuery that I was using for modal and create modal functions
    with vanilla JavaScript.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
-$(document).ready(function() {
-
-const directory = document.getElementById("directory");
-const search = document.getElementById("search");
-
-// connect to API and fetch data
-const connectToAPIs = () =>{
-  fetch('https://randomuser.me/api/?results=12&nat=us,au,br,ca,us,nz')
-  .then(response => response.json())
-  .then(data => {
-    const empList = data.results;
-    createEmployee(empList);
-    $('.modal').modaal();
-    searchEmp();
+  // connect to API and fetch data
+  const connectToAPIs = () => {
+    fetch('https://randomuser.me/api/?results=12&nat=us,au,br,ca,us,nz')
+    .then(response => response.json())
+    .then(data => {
+      createEmployee(data.results);
+      searchEmp();
+      modalDisplay();
   })
 };
 
-const createEmployee = (emp) =>{
-  let empHTML = '';
-  emp.forEach( emp =>{
-    const empName = emp.name.first+emp.name.last
-    empHTML += `<a href="#${empName}" class="modal" data-group="modal">
-                  <div class="employee">
+  // to create html elements for employee by using data from API
+  const createEmployee = (empArr) =>{
+    let empHTML = '';
+    empArr.forEach( emp => {
+    const empName = emp.name.first+emp.name.last;
+    empHTML += `  <div class="employee">
                     <img src="${emp.picture.medium}" alt="${empName}" class="img">
                     <div class="right">
                       <h3 class="name">${empName}</h3>
@@ -28,44 +22,50 @@ const createEmployee = (emp) =>{
                       <p class="place">${emp.location.city}</p>
                     </div>
                   </div>
-                  <div id="${empName}" style="display:none;">
-                    <div class="modalContainer">
+                  <div class="modalContainer">
+
+                    <span class="prev span">&larr;</span>
+                    <div class="modalContent">
+                      <span class="close span">&times;</span>
                       <img src="${emp.picture.medium}" alt="${empName}" class="img">
                       <h3 class="nameModal">${empName}</h3>
                       <p class="emailModal">${emp.email}</p>
                       <p class="placeModal">${emp.location.city}</p>
                       <hr>
                       <p class="cell">${emp.cell}</p>
-                      <p class="address">${emp.location.street},${emp.location.state}, ${emp.location.postcode}</p>
+                      <p class="address">${emp.location.street},${emp.location.state},${emp.location.postcode}</p>
                       <p class="birthday">Birthday: ${emp.dob.date.slice(5,7)}/${emp.dob.date.slice(8,10)}/${emp.dob.date.slice(0,4)}</p>
                     </div>
-                  </div>
-                </a>`;
+                    <span class="next span">&rarr;</span>
+
+                  </div>`;
   });
-  directory.innerHTML = empHTML;
+  document.getElementById('directory').innerHTML = empHTML;
 }
 
-const searchEmp = () => {
+  // when a user types something in search input
+  const searchEmp = () => {
 
-  search.addEventListener('keyup', (event)=>{
+    const search = document.getElementById('search');
+
+    search.addEventListener('keyup',(event)=>{
     event.preventDefault();
-    const name = document.getElementsByClassName('name');
-    const searchedWord = search.value.toUpperCase();
-    const href = document.getElementsByClassName('modal');
 
     // get employee's name from objects
+    const name = document.getElementsByClassName('name');
     const empArrForSearch = Object.keys(name).map((val,index) => {
       return name[index].innerHTML.toUpperCase();
     });
 
     // search function
+    const searchedWord = search.value.toUpperCase();
+    const emp = document.getElementsByClassName('employee');
     empArrForSearch.forEach((val,index)=>{
 
       if( val.indexOf(searchedWord) > -1 ){
-        href[index].style.display = 'block';
+        emp[index].style.display = 'flex';
       }else{
-        href[index].style.display = 'none';
-
+        emp[index].style.display = 'none';
       }
 
     });
@@ -73,6 +73,85 @@ const searchEmp = () => {
   });
 }
 
-connectToAPIs();
+  // when a user clicks employee
+  const modalDisplay = () => {
 
- });
+    // variables for click event trigger
+    const employee = document.getElementsByClassName('employee');
+    const close = document.getElementsByClassName('close');
+    const modal = document.getElementsByClassName('modalContainer');
+    const prev = document.getElementsByClassName('prev');
+    const next = document.getElementsByClassName('next');
+
+    // ------------------------- function for DRY ---------------------------
+    const displayModal = ele => {
+      ele.style.display="flex";
+      ele.style.flexDirection="row";
+      ele.style.alignItems="center";
+      ele.style.justifyContent="space-evenly";
+    };
+
+    const displayNone = ele => ele.style.display="none";
+    // ------------------------- function for DRY ---------------------------
+
+
+    Object.keys(employee).forEach((val,index)=>{
+
+      const selector = modal[index];
+      //display modal for each employee
+      employee[index].addEventListener('click',e => {
+        e.preventDefault();
+        displayModal(selector);
+      });
+
+      // close btn to close modal when it's clicked
+      close[index].addEventListener('click', e =>{
+        e.preventDefault();
+        displayNone(selector);
+      });
+
+      // modalContainer to close modal when it's clicked
+      modal[index].addEventListener('click', e =>{
+        e.preventDefault();
+        displayNone(selector);
+      });
+
+      // variables for prev btn and next btn
+      const lastSelector = modal[modal.length-1];
+      const firstSelector = modal[0];
+      const prevSelector = modal[index-1];
+      const nextvSelector = modal[index+1];
+
+      // to display preveous employee modal when it's clicked
+      prev[index].addEventListener('click', e =>{
+        e.preventDefault();
+
+        if(index!==0){
+          displayNone(selector);
+          displayModal(prevSelector)
+        }else{
+          displayNone(selector);
+          displayModal(lastSelector);
+        }
+      }); //prev btn
+
+
+      // to display next employee modal when it's clicked
+      next[index].addEventListener('click', e =>{
+        e.preventDefault();
+
+        if(index===modal.length-1){
+          displayNone(lastSelector);
+          displayModal(firstSelector);
+        }else{
+          displayNone(selector);
+          displayModal(nextvSelector);
+        }
+
+      }); //next btn
+
+    }); //forEach method
+
+  } //modalDisplay
+
+  connectToAPIs();
